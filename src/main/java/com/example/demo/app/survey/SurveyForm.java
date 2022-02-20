73040e2754365deb91671eb66a1e79b63f928bc2
keigom2020 <qlpt2020@gmail.com>
2022-02-20 14:19:13 +0900

    add: surveyのControllerなどを追加

--- src/main/java/com/example/demo/app/survey/SurveyForm.java
+++ src/main/java/com/example/demo/app/survey/SurveyForm.java
@@ -1,52 +1,48 @@
 package com.example.demo.app.survey;
 
+import javax.validation.constraints.Size;
+
 public class SurveyForm{
 
 	/*Add parameters(0~150) 引数を追加(0~150)*/
-    private int age;
+	@Size(min = 0, max = 150, message="Please input 150 characters or less")
+	private int age;
 
-    /*Add parameters(1~5) 引数を追加(1~5)*/
-    private int satisfaction;
+	/*Add parameters(1~5) 引数を追加(1~5)*/
+	@Size(min = 1, max = 5, message="Please input 5 characters or less")
+	private int satisfaction;
 
-    /*Add parameters(200 characters or less) 引数を追加(200文字以内)*/
-    private String comment;
+	/*Add parameters(200 characters or less) 引数を追加(200文字以内)*/
+	@Size(min = 0, max = 200, message="Please input 200 characters or less")
+	private String comment;
 
-	public SurveyForm() {}
-
-    public SurveyForm(int age, int satisfaction, String comment) {
+  public SurveyForm(int age, int satisfaction, String comment) {
 		this.age = age;
 		this.satisfaction = satisfaction;
 		this.comment = comment;
 	}
-
 
 	public int getAge() {
 		return age;
 	}
 
-
 	public void setAge(int age) {
 		this.age = age;
 	}
-
 
 	public int getSatisfaction() {
 		return satisfaction;
 	}
 
-
 	public void setSatisfaction(int satisfaction) {
 		this.satisfaction = satisfaction;
 	}
-
 
 	public String getComment() {
 		return comment;
 	}
 
-
 	public void setComment(String comment) {
 		this.comment = comment;
 	}
-
 }
