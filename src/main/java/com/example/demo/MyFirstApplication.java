c0b417efadbda416b5cd6da908f20881a4c9ddb9
keigom2020 <qlpt2020@gmail.com>
2022-02-20 12:23:20 +0900

    create: サンプルのSpringBootプロジェクトの作成：Javaファイル追加

---
+++ src/main/java/com/example/demo/MyFirstApplication.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstApplication {
+  
+  public static void main(String[] args) {
+    SpringApplication.run(MyFirstApplication.class args);
+  }
+}
